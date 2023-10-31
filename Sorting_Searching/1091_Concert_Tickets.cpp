61e6510d505bc3d1479663748eaa4c684162cf00
wulu <wulu90@outlook.com>
2023-11-01 01:26:37 +0800

    1091 fast than prev edition but slow,more than 1s

--- Sorting_Searching/1091_Concert_Tickets.cpp
+++ Sorting_Searching/1091_Concert_Tickets.cpp
@@ -1,42 +1,55 @@
+#include <algorithm>
 #include <iostream>
-#include <queue>
+#include <unordered_set>
 #include <vector>
 
 int main() {
-    int n, m, h;
+    int n, m;
     std::cin >> n >> m;
 
-    std::priority_queue<int> tickets;
+    std::vector<int> tickets(n);
+
     std::vector<int> customer_willings(m);
 
     for (int i = 0; i < n; i++) {
-        std::cin >> h;
-        tickets.push(h);
+        std::cin >> tickets[i];
     }
+    std::sort(tickets.begin(), tickets.end());
 
     for (int i = 0; i < m; i++) {
         std::cin >> customer_willings[i];
     }
 
-    std::vector<int> overs;
+    std::unordered_set<int> sold;
+
+    int min_price = tickets[0];
+    int min_index = 0;
+
     for (size_t i = 0; i < customer_willings.size(); i++) {
-        bool buyticket = false;
-        while (!tickets.empty()) {
-            if (tickets.top() > customer_willings[i]) {
-                overs.push_back(tickets.top());
-                tickets.pop();
-            } else {
-                std::cout << tickets.top() << std::endl;
-                tickets.pop();
-                buyticket = true;
-                break;
-            }
-        }
-        if (!buyticket) {
+        if (sold.size() == static_cast<size_t>(n) || customer_willings[i] < min_price) {
             std::cout << -1 << std::endl;
-        }
-        for (auto t : overs) {
-            tickets.push(t);
+            continue;
+        } else {
+            auto lb  = std::upper_bound(tickets.begin(), tickets.end(), customer_willings[i]);
+            int dist = lb - tickets.begin();
+            for (int j = dist - 1; j >= 0; j--) {
+                if (!sold.contains(j) && tickets[j] <= customer_willings[i]) {
+                    std::cout << tickets[j] << std::endl;
+                    sold.insert(j);
+
+                    if (tickets[j] == min_price) {
+                        for (int k = min_index; k < n; k++) {
+                            if (!sold.contains(k)) {
+                                min_price = tickets[k];
+                                min_index = k;
+                                break;
+                            }
+                        }
+                    }
+
+                    break;
+                }
+            }
         }
     }
 
