3700b7422dfd8bd6a0db13ece4473cfc72948afa
wulu <wulu90@outlook.com>
2023-11-01 03:04:46 +0800

    1091 g

--- Sorting_Searching/1091_Concert_Tickets.cpp
+++ Sorting_Searching/1091_Concert_Tickets.cpp
@@ -1,55 +1,31 @@
-#include <algorithm>
 #include <iostream>
-#include <unordered_set>
+#include <set>
 #include <vector>
 
 int main() {
-    int n, m;
+    int n, m, h;
     std::cin >> n >> m;
 
-    std::vector<int> tickets(n);
+    std::multiset<int> tickets;
 
     std::vector<int> customer_willings(m);
 
     for (int i = 0; i < n; i++) {
-        std::cin >> tickets[i];
+        std::cin >> h;
+        tickets.insert(h);
     }
-    std::sort(tickets.begin(), tickets.end());
 
     for (int i = 0; i < m; i++) {
         std::cin >> customer_willings[i];
     }
 
-    std::unordered_set<int> sold;
-
-    int min_price = tickets[0];
-    int min_index = 0;
-
-    for (size_t i = 0; i < customer_willings.size(); i++) {
-        if (sold.size() == static_cast<size_t>(n) || customer_willings[i] < min_price) {
+    for (int i = 0; i < m; i++) {
+        auto it = tickets.upper_bound(customer_willings[i]);
+        if (it == tickets.begin()) {
             std::cout << -1 << std::endl;
-            continue;
         } else {
-            auto lb  = std::upper_bound(tickets.begin(), tickets.end(), customer_willings[i]);
-            int dist = lb - tickets.begin();
-            for (int j = dist - 1; j >= 0; j--) {
-                if (!sold.contains(j) && tickets[j] <= customer_willings[i]) {
-                    std::cout << tickets[j] << std::endl;
-                    sold.insert(j);
-
-                    if (tickets[j] == min_price) {
-                        for (int k = min_index; k < n; k++) {
-                            if (!sold.contains(k)) {
-                                min_price = tickets[k];
-                                min_index = k;
-                                break;
-                            }
-                        }
-                    }
-
-                    break;
-                }
-            }
+            std::cout << *(--it) << std::endl;
+            tickets.erase(it);
         }
     }
 
