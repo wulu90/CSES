7b33b6906bd456bdd1c9b01f332f5a9dff1ec385
wulu <wulu90@outlook.com>
2023-11-07 10:38:34 +0800

    1646 2

--- Range_Queries/1646_Static_Range_Sum_Queries.cpp
+++ Range_Queries/1646_Static_Range_Sum_Queries.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
 int main() {
     int n, q, a, b;
     cin >> n >> q;
-    vector<int> vec(n + 1);
+    vector<int64_t> vec(n + 1);
+
     for (int i = 1; i <= n; i++) {
         cin >> vec[i];
     }
+
     vector<pair<int, int>> queries;
+
     for (int i = 0; i < q; i++) {
         cin >> a >> b;
         queries.push_back({a, b});
     }
 
-    for (auto& [x1, x2] : queries) {
-        int64_t sum = 0;
-        for (int i = x1; i <= x2; i++) {
-            sum += vec[i];
-        }
-        cout << sum << endl;
+    vector<int64_t> prefixsum(n + 1);
+
+    for (int i = 1; i <= n; i++) {
+        prefixsum[i] = prefixsum[i - 1] + vec[i];
     }
+
+    for (auto& [l, r] : queries) {
+        cout << prefixsum[r] - prefixsum[l - 1] << endl;
+    }
+
     return 0;
 }
